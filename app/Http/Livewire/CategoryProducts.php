82dbb5147002b870cfd6e2cd42da9696d2799fc4
Humberto Suárez <humbertosc2711@gmail.com>
2022-01-19 15:36:00 +0000

    FIN VIDEO 34

--- app/Http/Livewire/CategoryProducts.php
+++ app/Http/Livewire/CategoryProducts.php
@@ -7,6 +7,14 @@ use Livewire\Component;
 class CategoryProducts extends Component
 {
     public $category;
+    public $products = [];
+
+    public function loadProducts()
+    {
+        $this->products = $this->category->products()->where('status', 2)->take(15)->get();
+        $this->emit('glider', $this->category->id);
+    }
+
     public function render()
     {
         return view('livewire.category-products');
