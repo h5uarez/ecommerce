cead079fc6c40e550e50eff5a2c534aa3ccfc653
Humberto Suárez <humbertosc2711@gmail.com>
2022-02-16 00:46:08 +0100

    Arreglos Paypal2

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -6,13 +6,6 @@ use App\Models\Order;
 
 class OrderController extends Controller
 {
-    public function payment(Order $order)
-    {
-        $items = json_decode($order->content);
-
-        return view('orders.payment', compact('order', 'items'));
-    }
-
     public function show(Order $order)
     {
         return view('orders.show', compact('order'));
