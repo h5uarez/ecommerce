b7e0cb9cc96cd1802a2841a03b56a052ebfe03f9
Humberto Suárez <humbertosc2711 @gmail.com>
2022-02-20 16:51:07 +0100

    Fin paso 131

--- routes/admin.php
+++ routes/admin.php
@@ -3,6 +3,7 @@
 use App\Http\Livewire\Admin\ShowProducts;
 use Illuminate\Support\Facades\Route;
 use App\Http\Livewire\Admin\CreateProduct;
+use App\Http\Livewire\Admin\EditProduct;
 
 Route::get('/', ShowProducts::class)->name('admin.index');
 
@@ -13,4 +14,7 @@ Route::get('products/create', function () {
 })->name('admin.products.create');
 
 Route::get('products/create', CreateProduct::class)->name('admin.products.create');
+
+Route::get('products/{product}/edit', EditProduct::class)->name('admin.products.edit');
+
 
