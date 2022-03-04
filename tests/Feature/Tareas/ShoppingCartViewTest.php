01d7f9e39a12b3a4d12f5fa9ec1c4f0d75620ef2
Humberto Suárez <humbertosc2711@gmail.com>
2022-03-04 20:18:15 +0100

    Ejercicio 2 Test completado

--- tests/Feature/Tareas/ShoppingCartViewTest.php
+++ tests/Feature/Tareas/ShoppingCartViewTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Tareas;
 
+use App\CreateData;
 use Tests\TestCase;
 use App\Models\User;
 use App\Models\Brand;
@@ -12,6 +13,8 @@ use App\Models\Product;
 use App\Models\Category;
 use App\Models\Subcategory;
 use App\Http\Livewire\Search;
+use App\Listeners\MergeTheCart;
+use Illuminate\Auth\Events\Login;
 use App\Http\Livewire\AddCartItem;
 use App\Http\Livewire\ShoppingCart;
 use App\Http\Livewire\UpdateCartItem;
@@ -22,6 +25,7 @@ class ShoppingCartViewTest extends TestCase
 {
     use DatabaseMigrations;
     use CreateProduct;
+    use CreateData;
 
     //7- Al acceder a la vista del carrito, comprobar que podemos ver todos los items que tenga.
 
@@ -120,25 +124,61 @@ class ShoppingCartViewTest extends TestCase
 
 
     //11- Comprobar que el carrito se guarda en la BD cuando se cierra sesión y se recupera en caso de iniciar sesión y exista.
-
+    //Pregunta 2 del exámen
     /** @test */
     public function the_cart_is_saved_when_you_log_out()
     {
-        $product1 = $this->createProduct();
+        $product1 = $this->createData(false, false, 15);
+        $product2 = $this->createData(false, false, 14);
+        $product3 = $this->createData(false, false, 13);
+        $product4 = $this->createData(false, false, 12);
+
+
         $user = User::factory()->create();
         $this->actingAs($user);
 
         Livewire::test(AddCartItem::class, ['product' => $product1])
             ->call('addItem', $product1);
 
+        Livewire::test(AddCartItem::class, ['product' => $product2])
+            ->call('addItem', $product2);
+
+        Livewire::test(AddCartItem::class, ['product' => $product3])
+            ->call('addItem', $product3);
+
         $this->get('/orders/create')
             ->assertStatus(200)
-            ->assertSee($product1->name);
+            ->assertSee($product1->name)
+            ->assertSee($product2->name)
+            ->assertSee($product3->name)
+            ->assertDontSee($product4->name);
+
+        $price1 = $product1->price;
+        $price2 = $product2->price;
+        $price3 = $product3->price;
 
         $content = Cart::content();
         $this->post('logout');
-        $this->actingAs($user);
         $this->assertDatabaseHas('shoppingcart', ['content' => serialize($content)]);
+
+        $cback = new MergeTheCart();
+        $relog = new Login('web', $user, true);
+        $this->actingAs($user);
+
+        $cback->handle($relog);
+
+        $this->get('/orders/create')
+            ->assertStatus(200)
+            ->assertSee($product1->name)
+            ->assertSee($price1)
+            ->assertOk($product1->quantity = 15)
+            ->assertSee($product2->name)
+            ->assertSee($price2)
+            ->assertOk($product2->quantity = 14)
+            ->assertSee($product3->name)
+            ->assertSee($price3)
+            ->assertOk($product2->quantity = 13)
+            ->assertDontSee($product4->name);
     }
 
 
